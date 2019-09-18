663527198dd01d55eae435ae1c2959c60c699408
mqtsuo02 <obu.tokyo@gmail.com>
2019-09-18 23:54:36 +0900

    Added bad answer

---
+++ Go/p0013_low.go
@@ -0,0 +1,39 @@
+package leetcode
+
+import (
+	"sort"
+	"strconv"
+)
+
+func threeSum(nums []int) [][]int {
+	ns := nums
+	sort.Ints(ns)
+	answer := [][]int{}
+	length := len(ns)
+	m := make(map[string]bool)
+	for i := 0; i < length-2; i++ {
+		for j := i + 1; j < length-1; j++ {
+			for k := j + 1; k < length; k++ {
+				if ns[i]+ns[j]+ns[k] == 0 {
+					l := []int{nums[i], nums[j], nums[k]}
+					ls := nsToString(l)
+					if _, ok := m[ls]; !ok {
+						answer = append(answer, l)
+						m[ls] = true
+					}
+				}
+			}
+		}
+	}
+	return answer
+}
+
+func nsToString(ns []int) string {
+	s := ""
+	d := ""
+	for _, n := range ns {
+		s = s + strconv.Itoa(n) + d
+		d = ","
+	}
+	return s
+}
